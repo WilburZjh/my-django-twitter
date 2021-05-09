d3c5e64e974694fa4b4ee786b1bf29463f9b73bc
wilburzjh <wilburzjh@gmail.com>
2021-05-08 22:00:59 -0400

    3-install-django-rest-framework

---
+++ accounts/api/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import permissions
+from accounts.api.serializers import UserSerializer
+
+# design an User API to view and modify User table in DataBase.
+# /api/user/ -> all users
+# /api/user/id/ -> id user
+class UserViewSet(viewsets.ModelViewSet):
+    # 1.怎么渲染数据（数据怎么变成json），
+    # 2.有一个默认的表单，根据这个serializer去创建，这样在Django-rest-framework这个界面去进行数据的提交
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
