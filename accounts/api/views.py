467d49457de1990d130e2d1dc1a93aee7610ff0d
wilburzjh <wilburzjh@gmail.com>
2021-05-08 22:20:06 -0400

    add more comments in the views.py file

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -6,6 +6,7 @@ from accounts.api.serializers import UserSerializer
 # design an User API to view and modify User table in DataBase.
 # /api/user/ -> all users
 # /api/user/id/ -> id user
+# permission_classes: 对操作进行检测，检测用户是否登录，若没有登录则无法进行任何操作。
 class UserViewSet(viewsets.ModelViewSet):
     # 1.怎么渲染数据（数据怎么变成json），
     # 2.有一个默认的表单，根据这个serializer去创建，这样在Django-rest-framework这个界面去进行数据的提交
