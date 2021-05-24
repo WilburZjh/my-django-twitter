2e3d3605d6aa934b1c1e806c7d530e7955135408
wilburzjh <wilburzjh@gmail.com>
2021-05-24 15:06:49 -0400

    create a createcomment api for comments and test this api

---
+++ comments/api/tests.py
@@ -0,0 +1,53 @@
+from testing.testcases import TestCase
+from rest_framework.test import APIClient
+
+
+COMMENT_URL = '/api/comments/'
+
+
+class CommentApiTests(TestCase):
+
+    def setUp(self):
+        self.linghu = self.create_user('linghu')
+        self.linghu_client = APIClient()
+        self.linghu_client.force_authenticate(self.linghu)
+        self.dongxie = self.create_user('dongxie')
+        self.dongxie_client = APIClient()
+        self.dongxie_client.force_authenticate(self.dongxie)
+
+        self.tweet = self.create_tweet(self.linghu)
+
+    def test_create(self):
+        # 匿名不可以创建
+        response = self.anonymous_client.post(COMMENT_URL)
+        self.assertEqual(response.status_code, 403)
+
+        # 啥参数都没带不行
+        response = self.linghu_client.post(COMMENT_URL)
+        self.assertEqual(response.status_code, 400)
+
+        # 只带 tweet_id 不行
+        response = self.linghu_client.post(COMMENT_URL, {'tweet_id': self.tweet.id})
+        self.assertEqual(response.status_code, 400)
+
+        # 只带 content 不行
+        response = self.linghu_client.post(COMMENT_URL, {'content': '1'})
+        self.assertEqual(response.status_code, 400)
+
+        # content 太长不行
+        response = self.linghu_client.post(COMMENT_URL, {
+            'tweet_id': self.tweet.id,
+            'content': '1' * 141,
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual('content' in response.data['errors'], True)
+
+        # tweet_id 和 content 都带才行
+        response = self.linghu_client.post(COMMENT_URL, {
+            'tweet_id': self.tweet.id,
+            'content': '1',
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['user']['id'], self.linghu.id)
+        self.assertEqual(response.data['tweet_id'], self.tweet.id)
+        self.assertEqual(response.data['content'], '1')
