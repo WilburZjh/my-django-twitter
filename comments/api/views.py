eec291835566482c6075d3ff2c6e24597f012e9b
wilburzjh <wilburzjh@gmail.com>
2021-05-24 17:35:12 -0400

    create local settings file and list comments api

--- comments/api/views.py
+++ comments/api/views.py
@@ -17,6 +17,7 @@ class CommentViewSet(viewsets.GenericViewSet):
     """
     serializer_class = CommentSerializerForCreate
     queryset = Comment.objects.all()
+    filterset_fields = ('tweet_id',)
 
     # POST /api/comments/ -> create
     # GET /api/comments/ -> list
@@ -33,6 +34,30 @@ class CommentViewSet(viewsets.GenericViewSet):
         if self.action in ['update', 'destroy']:
             return [IsAuthenticated(), IsObjectOwner()]
         return [AllowAny()]
+
+    def list(self, request, *args, **kwargs):
+        # 指定删除某个tweet下的comments。
+        if 'tweet_id' not in request.query_params:
+            return Response({
+                    'message': 'missing tweet_id in request',
+                    'success': False,
+                }, status=status.HTTP_400_BAD_REQUEST,)
+        queryset = self.get_queryset()
+        comments = self.filter_queryset(queryset)\
+            .prefetch_related('user')\
+            .order_by('created_at')
+        serializer = CommentSerializer(comments, many=True)
+        return Response(
+            {'comments': serializer.data},
+            status=status.HTTP_200_OK,
+        )
+
+        # 不用filter的方式
+        # tweet_id = requests.query_params['tweet_id']
+        # comments = Comment.object.filter(tweet_id=tweet_id)
+        # serializer = CommentSerializer(comments, many=True)
+        # return Response(
+        #     {'comments': serializer.data}, status=status.HTTP_200_OK,)
 
     def create(self, request, *args, **kwargs):
         data = {
