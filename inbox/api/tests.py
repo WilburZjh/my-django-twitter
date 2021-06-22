ce6f6fd59dee10fa117a73328f213fc09c98d073
wilburzjh <wilburzjh@gmail.com>
2021-06-21 23:45:53 -0400

    notification-api part 1

--- inbox/api/tests.py
+++ inbox/api/tests.py
@@ -4,7 +4,7 @@ from testing.testcases import TestCase
 
 COMMENT_URL = '/api/comments/'
 LIKE_URL = '/api/likes/'
-
+NOTIFICATION_URL = '/api/notifications/'
 
 class NotificationTests(TestCase):
 
@@ -28,3 +28,96 @@ class NotificationTests(TestCase):
             'object_id': self.dongxie_tweet.id,
         })
         self.assertEqual(Notification.objects.count(), 1)
+
+class NotificationApiTests(TestCase):
+
+    def setUp(self):
+        self.linghu, self.linghu_client = self.create_user_and_client('linghu')
+        self.dongxie, self.dongxie_client = self.create_user_and_client('dongxie')
+        self.linghu_tweet = self.create_tweet(self.linghu)
+
+    def test_unread_count(self):
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'tweet',
+            'object_id': self.linghu_tweet.id,
+        })
+
+        url = '/api/notifications/unread-count/'
+        response = self.linghu_client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['unread_count'], 1)
+
+        comment = self.create_comment(self.linghu, self.linghu_tweet)
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'comment',
+            'object_id': comment.id,
+        })
+        response = self.linghu_client.get(url)
+        self.assertEqual(response.data['unread_count'], 2)
+        response = self.dongxie_client.get(url)
+        self.assertEqual(response.data['unread_count'], 0)
+    def test_mark_all_as_read(self):
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'tweet',
+            'object_id': self.linghu_tweet.id,
+        })
+        comment = self.create_comment(self.linghu, self.linghu_tweet)
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'comment',
+            'object_id': comment.id,
+        })
+
+        unread_url = '/api/notifications/unread-count/'
+        response = self.linghu_client.get(unread_url)
+        self.assertEqual(response.data['unread_count'], 2)
+
+        mark_url = '/api/notifications/mark-all-as-read/'
+        response = self.linghu_client.get(mark_url)
+        self.assertEqual(response.status_code, 405)
+
+        # dongxie cannot mark linghu's notification as read
+        response = self.dongxie_client.post(mark_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['marked_count'], 0)
+        response = self.linghu_client.get(unread_url)
+        self.assertEqual(response.data['unread_count'], 2)
+
+        # linghu can mark his notifications as read
+        response = self.linghu_client.post(mark_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['marked_count'], 2)
+        response = self.linghu_client.get(unread_url)
+        self.assertEqual(response.data['unread_count'], 0)
+
+    def test_list(self):
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'tweet',
+            'object_id': self.linghu_tweet.id,
+        })
+        comment = self.create_comment(self.linghu, self.linghu_tweet)
+        self.dongxie_client.post(LIKE_URL, {
+            'content_type': 'comment',
+            'object_id': comment.id,
+        })
+
+        # 匿名用户无法访问 api
+        response = self.anonymous_client.get(NOTIFICATION_URL)
+        self.assertEqual(response.status_code, 403)
+        # dongxie 看不到任何 notifications
+        response = self.dongxie_client.get(NOTIFICATION_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['count'], 0)
+        # linghu 看到两个 notifications
+        response = self.linghu_client.get(NOTIFICATION_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['count'], 2)
+        # 标记之后看到一个未读
+        notification = self.linghu.notifications.first()
+        notification.unread = False
+        notification.save()
+        response = self.linghu_client.get(NOTIFICATION_URL)
+        self.assertEqual(response.data['count'], 2)
+        response = self.linghu_client.get(NOTIFICATION_URL, {'unread': True})
+        self.assertEqual(response.data['count'], 1)
+        response = self.linghu_client.get(NOTIFICATION_URL, {'unread': False})
+        self.assertEqual(response.data['count'], 1)
--- twitter/urls.py
+++ twitter/urls.py
@@ -22,6 +22,7 @@ from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from comments.api.views import CommentViewSet
 from likes.api.views import LikeViewSet
+from inbox.api.views import NotificationViewSet
 from django.conf import settings
 
 router = routers.DefaultRouter()
@@ -32,6 +33,7 @@ router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
 router.register(r'api/likes', LikeViewSet, basename='likes')
+router.register(r'api/notifications', NotificationViewSet, basename='notifications')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
